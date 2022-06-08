9345c4f0991b5c842058472fda28460a4304db6a
ino <littletasteofheaven@protonmail.com>
2022-06-08 19:09:46 +0600

    Place navigation items in a separate file

---
+++ app/src/main/java/bm/app/data/navItems.kt
@@ -0,0 +1,13 @@
+package bm.app.data
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import bm.app.navigation.NavItem
+
+val navItems = listOf(
+    NavItem(Icons.Filled.Home, "Home"),
+    NavItem(Icons.Filled.Email, "Mail"),
+    NavItem(Icons.Filled.Person, "Profile")
+)
