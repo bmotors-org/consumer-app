862b13501810e0991cf7b400ce79e82a0ffa4419
ino <littletasteofheaven@protonmail.com>
2022-06-08 09:12:01 +0600

    Replace Dialog with AlertDialog

--- app/src/main/java/bm/app/screens/Service.kt
+++ app/src/main/java/bm/app/screens/Service.kt
@@ -3,33 +3,33 @@ package bm.app.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.SecureFlagPolicy
 import androidx.datastore.preferences.core.stringPreferencesKey
+import bm.app.R
 import bm.app.dataStore
 import kotlinx.coroutines.flow.map
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Service(categoryName: String) {
     val dataStore = LocalContext.current.dataStore
@@ -91,66 +91,68 @@ fun Service(categoryName: String) {
 
     when (verifyPhoneCardSt) {
         true -> {
-            Dialog(
-                onDismissRequest = { setVerifyPhoneCardSt(false) }
-            ) {
-                Card(
-                    shape = RoundedCornerShape(4.dp),
-                ) {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.spacedBy(16.dp),
-                        modifier = Modifier.padding(24.dp, 16.dp)
+            AlertDialog(
+                onDismissRequest = { setVerifyPhoneCardSt(false) },
+                confirmButton = {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        contentPadding = PaddingValues(16.dp, 10.dp)
                     ) {
                         Text(
-                            text = "Enter the otp sent to your phone number",
-                            fontSize = 20.sp,
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier.fillMaxWidth()
+                            text = "Verify",
                         )
-                        OutlinedTextField(
-                            value = otpSt,
-                            onValueChange = { setOtpSt(it) },
-                            label = {
-                                Text(
-                                    text = "OTP"
-                                )
-                            },
-                            modifier = Modifier.fillMaxWidth()
-
-                        )
-                        Row(
-                            modifier = Modifier.fillMaxWidth()
-                                .padding(0.dp, 4.dp),
-                            horizontalArrangement = Arrangement.spacedBy(
-                                12.dp, alignment = Alignment.End
-                            )
-                        ) {
-                            Button(
-                                onClick = {
-                                    setVerifyPhoneCardSt(false)
-                                    setOtpSt("")
-                                },
-                                contentPadding = PaddingValues(16.dp, 10.dp)
-                            ) {
-                                Text(
-                                    text = "Cancel",
-                                    fontSize = 16.sp
-                                )
-                            }
-                            Button(
-                                onClick = { /*TODO*/ },
-                                contentPadding = PaddingValues(16.dp, 10.dp)
-                            ) {
-                                Text(
-                                    text = "Verify",
-                                    fontSize = 16.sp
-                                )
-                            }
-                        }
                     }
-                }
-            }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            setVerifyPhoneCardSt(false)
+                            setOtpSt("")
+                        },
+                        contentPadding = PaddingValues(16.dp, 10.dp)
+                    ) {
+                        Text(
+                            text = "Cancel",
+                        )
+                    }
+                },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onPrimaryContainer,
+                        modifier = Modifier.size(32.dp)
+                    )
+                },
+                title = {
+                    Text(
+                        text = "Enter the otp sent to your phone number",
+                    )
+                },
+                text = {
+                    OutlinedTextField(
+                        value = otpSt,
+                        onValueChange = { setOtpSt(it) },
+                        label = {
+                            Text(
+                                text = "OTP"
+                            )
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                },
+                shape = RoundedCornerShape(8.dp),
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                tonalElevation = 8.dp,
+                iconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                textContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                properties = DialogProperties(
+                    dismissOnBackPress = true,
+                    dismissOnClickOutside = true,
+                    securePolicy = SecureFlagPolicy.SecureOn
+                )
+            )
         }
 
         false -> {
