1c58cbb5c457344a6e4e04ab6b5ba245dff42841
ino <littletasteofheaven@protonmail.com>
2022-06-09 18:01:03 +0600

    Move api call functions into an object

---
+++ app/src/main/java/bm/app/ktor/ApiMethods.kt
@@ -0,0 +1,23 @@
+package bm.app.ktor
+
+import bm.app.data.constants.ApiEndPoints
+import bm.app.data.serde.OtpVerification
+import bm.app.data.serde.PhoneVerification
+import io.ktor.client.request.* // ktlint-disable no-wildcard-imports
+import io.ktor.client.statement.* // ktlint-disable no-wildcard-imports
+
+object ApiMethods {
+    suspend fun verifyPhoneNumber(phoneNumber: String): HttpResponse {
+        return KtorHttpClient.post {
+            url(urlString = ApiEndPoints.VERIFY_PHONE)
+            setBody(PhoneVerification(phoneNumber))
+        }
+    }
+
+    suspend fun verifyOtp(phoneNumber: String, otp: String): HttpResponse {
+        return KtorHttpClient.post {
+            url(urlString = ApiEndPoints.VERIFY_OTP)
+            setBody(OtpVerification(phoneNumber, otp))
+        }
+    }
+}
