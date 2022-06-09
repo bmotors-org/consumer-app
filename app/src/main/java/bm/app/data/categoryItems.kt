b3e6a7ed0dd5380d4730c37ab8d9c524d1819819
ino <littletasteofheaven@protonmail.com>
2022-06-10 01:44:24 +0600

    Change the image to display

--- app/src/main/java/bm/app/data/categoryItems.kt
+++ app/src/main/java/bm/app/data/categoryItems.kt
@@ -6,7 +6,7 @@ data class Logo(val id: Int, val desc: String)
 
 data class Category(val logo: Logo, val name: String)
 
-const val truckLogoID = R.drawable.categoryplaceholder
+const val truckLogoID = R.drawable.logo
 
 val categoryList = listOf(
     Category(Logo(truckLogoID, "Truck"), "Open Truck"),
