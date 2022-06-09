15f7160b6a5591d342c1b2174b0c799f147ef2b3
ino <littletasteofheaven@protonmail.com>
2022-06-09 09:21:42 +0600

    Create serializable data class

---
+++ app/src/main/java/bm/app/data/serde/OtpVerification.kt
@@ -0,0 +1,8 @@
+package bm.app.data.serde
+
+import io.ktor.resources.* // ktlint-disable no-wildcard-imports
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Resource(path = "/verify-phone-number")
+data class OtpVerification(val otpCode: String, val phoneNumber: String)
