10ed3f1f94903ba968ac1547820dc6a6837cb3d7
ino <littletasteofheaven@protonmail.com>
2022-06-11 10:04:26 +0600

    Catch various errors to not crash the app

--- app/src/main/java/bm/app/ktor/ApiMethods.kt
+++ app/src/main/java/bm/app/ktor/ApiMethods.kt
@@ -3,21 +3,47 @@ package bm.app.ktor
 import bm.app.data.constants.ApiEndPoints
 import bm.app.data.serde.OtpVerification
 import bm.app.data.serde.PhoneVerification
-import io.ktor.client.request.* // ktlint-disable no-wildcard-imports
-import io.ktor.client.statement.* // ktlint-disable no-wildcard-imports
+import bm.app.ktor.utils.CustomHttpResponse
+import bm.app.ktor.utils.KtorResponseException
+import bm.app.ktor.utils.MissingPageException
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import java.net.ConnectException
 
 object ApiMethods {
     suspend fun verifyPhoneNumber(phoneNumber: String): HttpResponse {
-        return KtorHttpClient.post {
-            url(urlString = ApiEndPoints.VERIFY_PHONE)
-            setBody(PhoneVerification(phoneNumber))
+        return try {
+            KtorHttpClient.post {
+                url(urlString = ApiEndPoints.VERIFY_PHONE)
+                setBody(PhoneVerification(phoneNumber))
+            }
+        } catch (cause: KtorResponseException) {
+            println(cause.message)
+            cause.response
+        } catch (cause: MissingPageException) {
+            println(cause.message)
+            cause.response
+        } catch (cause: ConnectException) {
+            println(cause.message)
+            CustomHttpResponse(500, "Server Error")
         }
     }
 
     suspend fun verifyOtp(phoneNumber: String, otpCode: String): HttpResponse {
-        return KtorHttpClient.post {
-            url(urlString = ApiEndPoints.VERIFY_OTP)
-            setBody(OtpVerification(phoneNumber, otpCode))
+        return try {
+            KtorHttpClient.post {
+                url(urlString = ApiEndPoints.VERIFY_OTP)
+                setBody(OtpVerification(phoneNumber, otpCode))
+            }
+        } catch (cause: KtorResponseException) {
+            println(cause.message)
+            cause.response
+        } catch (cause: MissingPageException) {
+            println(cause.message)
+            cause.response
+        } catch (cause: ConnectException) {
+            println(cause.message)
+            CustomHttpResponse(500, "Server Error")
         }
     }
 }
