b9d673514571cf7238e7ac18fa5e32696ea5e848
ino <littletasteofheaven@protonmail.com>
2022-06-12 20:11:22 +0600

    A component that shows verification confirmation message.

---
+++ app/src/main/java/bm/app/components/VerifyConfirmChip.kt
@@ -0,0 +1,25 @@
+package bm.app.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SuggestionChip
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun VerifyConfirmChip() {
+    SuggestionChip(
+        onClick = {
+            /*TODO*/
+        },
+        icon = {
+            Icon(imageVector = Icons.Filled.CheckCircle, contentDescription = null)
+        },
+        label = {
+            Text("Verified")
+        }
+    )
+}
