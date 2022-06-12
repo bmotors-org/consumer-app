5ec02aa5f75c55e005b3120bf5db2c03048cc43b
ino <littletasteofheaven@protonmail.com>
2022-06-12 18:56:57 +0600

    Add prefix text (bd country code), Number only keyboard and make phone number textfield read-only when user has been verified

--- app/src/main/java/bm/app/screens/service/Service.kt
+++ app/src/main/java/bm/app/screens/service/Service.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -12,9 +13,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.lifecycle.viewmodel.compose.viewModel
 import bm.app.components.OtpInputDialog
@@ -53,21 +59,38 @@ fun Service(
 
     Column(
         modifier = Modifier.fillMaxWidth().padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = categoryName,
-            style = MaterialTheme.typography.headlineLarge
+            style = MaterialTheme.typography.headlineLarge,
+            textAlign = TextAlign.Center
         )
         OutlinedTextField(
             value = phoneNumber,
             onValueChange = { setPhoneNumber(it) },
+            modifier = Modifier.fillMaxWidth(),
+            readOnly = verified,
+            textStyle = TextStyle(
+                fontSize = 18.sp,
+                letterSpacing = 1.sp
+            ),
             label = {
                 Text(
                     text = "Phone Number"
                 )
             },
-            modifier = Modifier.fillMaxWidth()
+            leadingIcon = {
+                Text(
+                    text = "+ 880",
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(12.dp, 0.dp, 4.dp, 0.dp)
+                )
+            },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number
+            )
         )
 
         when (verified) {
