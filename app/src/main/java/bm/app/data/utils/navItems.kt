4212bb2d38efab9efc733283aefdc958482e0f68
ino <littletasteofheaven@protonmail.com>
2022-06-14 20:27:43 +0600

    Add route field

--- app/src/main/java/bm/app/data/utils/navItems.kt
+++ app/src/main/java/bm/app/data/utils/navItems.kt
@@ -4,10 +4,16 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
-import bm.app.navigation.NavItem
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavItem(
+    val icon: ImageVector,
+    val label: String,
+    val route: String
+)
 
 val navItems = listOf(
-    NavItem(Icons.Filled.Home, "Home"),
-    NavItem(Icons.Filled.Email, "Mail"),
-    NavItem(Icons.Filled.Person, "Profile")
+    NavItem(Icons.Filled.Home, "Home", "home"),
+    NavItem(Icons.Filled.Email, "Mail", "mail"),
+    NavItem(Icons.Filled.Person, "Profile", "profile")
 )
