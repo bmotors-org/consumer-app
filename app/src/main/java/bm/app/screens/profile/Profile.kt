60505afd7d7c0fd1a1b308c8202cdba786e8d5db
ino <littletasteofheaven@protonmail.com>
2022-06-14 20:34:15 +0600

    WIP: Profile screen

---
+++ app/src/main/java/bm/app/screens/profile/Profile.kt
@@ -0,0 +1,9 @@
+package bm.app.screens.profile
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Profile() {
+    Text(text = "This is your profile")
+}
