4729d0715e1aa55b162edd6b1f33193e3d25f6ed
ino <littletasteofheaven@protonmail.com>
2022-06-15 02:54:08 +0600

    Add a method to get phone number from preferences storage

--- app/src/main/java/bm/app/screens/service/api/Storage.kt
+++ app/src/main/java/bm/app/screens/service/api/Storage.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.datastore.preferences.core.edit
 import bm.app.data.preferencesDS.DataStoreKeys
 import bm.app.dataStore
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 
 class Storage(
     context: Context
@@ -15,5 +17,11 @@ class Storage(
             it[DataStoreKeys.TOKEN] = token
             it[DataStoreKeys.PHONE_NUMBER] = phoneNumber
         }
+    }
+
+    suspend fun getPhoneNumber(): String {
+        return dataStore.data.map {
+            it[DataStoreKeys.PHONE_NUMBER] ?: ""
+        }.first()
     }
 }
