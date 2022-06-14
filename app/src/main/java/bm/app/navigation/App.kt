7ecb24eeb1bbda3875b3e7d080cd9be8cc11144c
ino <littletasteofheaven@protonmail.com>
2022-06-14 20:24:43 +0600

    Move a data class to data directory

--- app/src/main/java/bm/app/navigation/App.kt
+++ app/src/main/java/bm/app/navigation/App.kt
@@ -4,11 +4,8 @@ import android.annotation.SuppressLint
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.compose.rememberNavController
 import bm.app.components.BottomBar
-
-data class NavItem(val icon: ImageVector, val label: String)
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
