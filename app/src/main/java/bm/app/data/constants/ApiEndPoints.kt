e0a5163f705e99b5815856a0a7b71d6792ba23da
ino <littletasteofheaven@protonmail.com>
2022-06-14 20:23:30 +0600

    Add a temporary endpoint to test tokens

--- app/src/main/java/bm/app/data/constants/ApiEndPoints.kt
+++ app/src/main/java/bm/app/data/constants/ApiEndPoints.kt
@@ -3,4 +3,5 @@ package bm.app.data.constants
 object ApiEndPoints {
     const val VERIFY_PHONE = "auth/verify-phone-number"
     const val VERIFY_OTP = "auth/verify-otp"
+    const val TEST_BEARER = "auth/test-bearer"
 }
