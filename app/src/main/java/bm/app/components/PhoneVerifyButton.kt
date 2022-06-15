5ac880f46972469a2df05330fbf707ca6ae6c4f6
ino <littletasteofheaven@protonmail.com>
2022-06-15 17:40:33 +0600

    Minor changes

--- app/src/main/java/bm/app/components/PhoneVerifyButton.kt
+++ app/src/main/java/bm/app/components/PhoneVerifyButton.kt
@@ -8,8 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import io.ktor.client.statement.*
-import io.ktor.http.*
+import bm.app.screens.service.api.PhoneVerificationResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -17,7 +16,7 @@ import kotlinx.coroutines.launch
 fun PhoneVerifyButton(
     phoneNumber: String,
     setOtpInputDialogVisibility: (Boolean) -> Unit,
-    beginPhoneVerification: suspend (String) -> HttpResponse
+    beginPhoneVerification: suspend (String) -> PhoneVerificationResponse
 ) {
     val coroutineScope = rememberCoroutineScope()
 
@@ -25,7 +24,7 @@ fun PhoneVerifyButton(
         onClick = {
             coroutineScope.launch(Dispatchers.Default) {
                 val response = beginPhoneVerification(phoneNumber)
-                when (response.status == HttpStatusCode.InternalServerError) {
+                when (response.success) {
                     true -> {
                         // TODO: Implement later
                     }
