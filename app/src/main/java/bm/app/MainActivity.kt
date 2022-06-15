405a783727a075ce88b085461841b1f7367840e0
ino <littletasteofheaven@protonmail.com>
2022-06-15 23:07:55 +0600

    Remove redundancy from `MainActivity`

--- app/src/main/java/bm/app/MainActivity.kt
+++ app/src/main/java/bm/app/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
@@ -33,18 +30,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BMAppTheme {
-        Greeting("Android")
     }
 }
