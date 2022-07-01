23001ae390523371974846262ca855d0c4720dd8
ino <littletasteofheaven@protonmail.com>
2022-07-02 02:24:51 +0600

    Data types for network layer methods in profile screen

---
+++ app/src/main/java/bm/app/screens/profile/api/network/data/MergeNameReq.kt
@@ -0,0 +1,9 @@
+package bm.app.screens.profile.api.network.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class MergeNameReq(
+    val name: String,
+    val phoneNumber: String
+)
---
+++ app/src/main/java/bm/app/screens/profile/api/network/data/MergeNameRes.kt
@@ -0,0 +1,6 @@
+package bm.app.screens.profile.api.network.data
+
+class MergeNameRes(
+    val success: Boolean,
+    val message: String
+)
