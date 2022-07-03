304d689caa4f60da34cf4b090cd289c6b25ebeea
ino <littletasteofheaven@protonmail.com>
2022-07-03 19:54:35 +0600

    Add `Authorization` header

--- app/src/main/java/bm/app/screens/profile/api/network/Network.kt
+++ app/src/main/java/bm/app/screens/profile/api/network/Network.kt
@@ -5,13 +5,14 @@ import bm.app.screens.profile.api.network.data.MergeNameReq
 import bm.app.screens.profile.api.network.data.MergeNameRes
 import bm.app.screens.profile.api.network.resources.Profile
 import io.ktor.client.plugins.resources.post
-import io.ktor.client.request.setBody
+import io.ktor.client.request.*
 
 class Network {
-    suspend fun mergeName(name: String, token: String): MergeNameRes {
+    suspend fun mergeName(name: String, sessionID: String): MergeNameRes {
         return try {
             KtorHttpClient.post(resource = Profile.Name()) {
-                setBody(MergeNameReq(name, token))
+                header("Authorization", sessionID)
+                setBody(MergeNameReq(name))
             }
             MergeNameRes(success = true, message = "Merge name success")
         } catch (cause: Exception) {
