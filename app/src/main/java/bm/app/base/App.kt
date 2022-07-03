1524f8baf80a758af56b30883c4aeaf181218a7c
ino <littletasteofheaven@protonmail.com>
2022-07-03 19:53:37 +0600

    change in local package import

--- app/src/main/java/bm/app/base/App.kt
+++ app/src/main/java/bm/app/base/App.kt
@@ -4,7 +4,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
-import bm.app.components.BottomBar
+import bm.app.base.components.BottomBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
