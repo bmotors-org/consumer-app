4153a758468626483076ea48b564e62a1257f02a
ino <littletasteofheaven@protonmail.com>
2022-07-03 20:51:07 +0600

    Rename `getToken` to `readUserData`

--- app/src/main/java/bm/app/base/NavLogicViewModel.kt
+++ app/src/main/java/bm/app/base/NavLogicViewModel.kt
@@ -2,10 +2,10 @@ package bm.app.base
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import bm.app.base.api.Storage
+import bm.app.base.api.storage.Storage
 
 class NavLogicViewModel(application: Application) : AndroidViewModel(application) {
     private val storageApi = Storage(application)
 
-    suspend fun readToken() = storageApi.readToken()
+    suspend fun getUserData() = storageApi.getUserData()
 }
