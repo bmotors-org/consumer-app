1465d7020b5890b0c7b36e69e3b7992d83dc51a7
ino <littletasteofheaven@protonmail.com>
2022-07-04 17:03:51 +0600

    Add storage api to ProfileVM

--- app/src/main/java/bm/app/screens/profile/ProfileViewModel.kt
+++ app/src/main/java/bm/app/screens/profile/ProfileViewModel.kt
@@ -1,13 +1,23 @@
 package bm.app.screens.profile
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import bm.app.screens.profile.api.network.Network
+import bm.app.screens.profile.api.storage.Storage
 
-class ProfileViewModel : ViewModel() {
+class ProfileViewModel(
+    application: Application
+) : AndroidViewModel(application) {
     private val networkApi = Network()
+
+    private val storageApi = Storage(application)
 
     suspend fun mergeName(
         name: String,
         phoneNumber: String
     ) = networkApi.mergeName(name, phoneNumber)
+
+    suspend fun storeName(
+        name: String
+    ) = storageApi.storeName(name)
 }
