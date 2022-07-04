fd709d79c5292d39e628ce5fcf0528e98f47dba1
ino <littletasteofheaven@protonmail.com>
2022-07-05 02:19:12 +0600

    Add a method to merge email

--- app/src/main/java/bm/app/screens/profile/api/network/Network.kt
+++ app/src/main/java/bm/app/screens/profile/api/network/Network.kt
@@ -1,6 +1,8 @@
 package bm.app.screens.profile.api.network
 
 import bm.app.ktor.KtorHttpClient
+import bm.app.screens.profile.api.network.data.MergeEmailReq
+import bm.app.screens.profile.api.network.data.MergeEmailRes
 import bm.app.screens.profile.api.network.data.MergeNameReq
 import bm.app.screens.profile.api.network.data.MergeNameRes
 import bm.app.screens.profile.api.network.resources.Profile
@@ -18,6 +20,19 @@ class Network {
         } catch (cause: Exception) {
             println(cause)
             MergeNameRes(success = false, message = "Merge name failed")
+        }
+    }
+
+    suspend fun mergeEmail(email: String, sessionID: String): MergeEmailRes {
+        return try {
+            KtorHttpClient.post(resource = Profile.Email()) {
+                header("Authorization", sessionID)
+                setBody(MergeEmailReq(email))
+            }
+            MergeEmailRes(success = true, message = "Merge email success")
+        } catch (cause: Exception) {
+            println(cause)
+            MergeEmailRes(success = false, message = "Merge email failed")
         }
     }
 }
