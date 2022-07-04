77bc333c1793dcc9044e2d914b9e257a3d7df6f7
ino <littletasteofheaven@protonmail.com>
2022-07-04 17:07:21 +0600

    Misc

--- app/src/main/java/bm/app/screens/service/api/storage/Storage.kt
+++ app/src/main/java/bm/app/screens/service/api/storage/Storage.kt
@@ -4,24 +4,21 @@ import android.content.Context
 import androidx.datastore.preferences.core.edit
 import bm.app.data.preferencesDS.DataStoreKeys
 import bm.app.dataStore
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
 
 class Storage(
     context: Context
 ) {
     private val dataStore = context.dataStore
 
-    suspend fun storeCreds(phoneNumber: String, sessionID: String) {
+    suspend fun storeCreds(
+        phoneNumber: String,
+        sessionID: String,
+        name: String
+    ) {
         dataStore.edit {
             it[DataStoreKeys.SESSION_ID] = sessionID
             it[DataStoreKeys.PHONE_NUMBER] = phoneNumber
+            it[DataStoreKeys.NAME] = name
         }
-    }
-
-    suspend fun getPhoneNumber(): String {
-        return dataStore.data.map {
-            it[DataStoreKeys.PHONE_NUMBER] ?: ""
-        }.first()
     }
 }
