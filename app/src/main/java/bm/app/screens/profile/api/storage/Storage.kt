387f9e6a7e84e75b7f7ff84f7916ff326689d723
ino <littletasteofheaven@protonmail.com>
2022-07-05 02:17:07 +0600

    Add methods to store email and clean all user data

--- app/src/main/java/bm/app/screens/profile/api/storage/Storage.kt
+++ app/src/main/java/bm/app/screens/profile/api/storage/Storage.kt
@@ -17,9 +17,30 @@ class Storage(
             }
         } catch (cause: Exception) {
             println(
-                "Error saving name: ${cause.localizedMessage}"
+                "Error storing name: ${cause.localizedMessage}"
             )
         }
+    }
 
+    suspend fun storeEmail(email: String) {
+        try {
+            dataStore.edit {
+                it[DataStoreKeys.EMAIL] = email
+            }
+        } catch (cause: Exception) {
+            println(
+                "Error storing email: ${cause.localizedMessage}"
+            )
+        }
+    }
+
+    suspend fun cleanCreds() {
+        try {
+            dataStore.edit {
+                it.clear()
+            }
+        } catch (cause: Exception) {
+            println("Error cleaning creds from disk")
+        }
     }
 }
