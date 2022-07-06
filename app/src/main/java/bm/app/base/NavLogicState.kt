d55a2d685d1f1a8586383f8aa37785ba6a5759c1
ino <littletasteofheaven@protonmail.com>
2022-07-07 02:52:17 +0600

    make variables private

--- app/src/main/java/bm/app/base/NavLogicState.kt
+++ app/src/main/java/bm/app/base/NavLogicState.kt
@@ -3,9 +3,15 @@ package bm.app.base
 import androidx.compose.runtime.mutableStateOf
 
 class NavLogicState {
-    var verified = mutableStateOf(false)
-    var sessionID = mutableStateOf("")
-    var phoneNumber = mutableStateOf("")
-    var name = mutableStateOf("")
-    var email = mutableStateOf("")
+    private var verified = mutableStateOf(false)
+    private var sessionID = mutableStateOf("")
+    private var phoneNumber = mutableStateOf("")
+    private var name = mutableStateOf("")
+    private var email = mutableStateOf("")
+
+    operator fun component1() = verified
+    operator fun component2() = sessionID
+    operator fun component3() = phoneNumber
+    operator fun component4() = name
+    operator fun component5() = email
 }
