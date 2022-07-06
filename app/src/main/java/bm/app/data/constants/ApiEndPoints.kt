ed8ebab3f33e7f19193bb966b78e395aef1ef61e
ino <littletasteofheaven@protonmail.com>
2022-07-07 03:30:46 +0600

    Delete network endpoints variables, might return later

--- app/src/main/java/bm/app/data/constants/ApiEndPoints.kt
+++
@@ -1,7 +0,0 @@
-package bm.app.data.constants
-
-object ApiEndPoints {
-    const val VERIFY_PHONE = "auth/verify-phone-number"
-    const val VERIFY_OTP = "auth/verify-otp"
-    const val TEST_BEARER = "auth/test-bearer"
-}
